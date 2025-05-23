f7edb968329375cca8563ee1d28a3ac63bc98cbd
Sarah-Lena Woköck <slwokoeck@gmail.com>
2025-05-23 23:21:33 +0200

    replaced ai.icon with logo

--- features/chat-home-page/chat-home.tsx
+++ features/chat-home-page/chat-home.tsx
@@ -22,7 +22,7 @@ export const ChatHome: FC<ChatPersonaProps> = (props) => {
           title={
             <>
               <Image
-                src={"/ai-icon.png"}
+                src={"/logo.jpeg"}
                 width={60}
                 height={60}
                 quality={100}
--- features/reporting-page/reporting-chat-page.tsx
+++ features/reporting-page/reporting-chat-page.tsx
@@ -29,7 +29,7 @@ export default function ReportingChatPage(props: ReportingChatPageProps) {
                   navigator.clipboard.writeText(message.content);
                 }}
                 profilePicture={
-                  message.role === "assistant" ? "/ai-icon.png" : undefined
+                  message.role === "assistant" ? "/logo.jpeg" : undefined
                 }
               >
                 <MessageContent message={message} />
